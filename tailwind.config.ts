725d746f74802edbec23cb2949f16b49acb5254f
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-09-04 11:26:56 -0500

    Setting colors for the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,18 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'dark-grey': '#24232C',
+      'grey': '#817D92',
+      'almost-white': '#E6E5EA',
+      'very-dark-grey': '#18171F',
+      'neon-green': '#A4FFAF',
+      'red': '#F64A4A',
+      'orange': '#FB7C58',
+      'yellow': '#F8CD65'
+    }
   },
+  
   plugins: [],
 };
 export default config;
