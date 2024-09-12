d0ace5ab5bcc88375400d12c685e55b340fa8358
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-09-12 12:04:34 -0500

    Setting length for the password

--- src/app/components/Details.tsx
+++ src/app/components/Details.tsx
@@ -1,15 +1,25 @@
+"use client"
+
+import { ChangeEvent, ChangeEventHandler, useState } from "react";
 import Checkbox from "./Checkbox";
+import Strength from "./Strength";
 
 export default function Details() {
+
+    const [length, setLength] = useState<string>('10');
+
+    const handleLength = (event: ChangeEvent<HTMLInputElement>) => {
+        setLength(event.target.value);
+    }
+
     return (
         <section className="bg-dark-grey w-full mt-6 p-4">
             <div className="flex items-center justify-between">
-                <p className="p">Character Length</p>
-                <h1 className="heading-l text-neon-green">10</h1>
+                <p className="p text-almost-white">Character Length</p>
+                <h1 className="heading-l text-neon-green">{length}</h1>
             </div>
-            <div className="w-full bg-very-dark-grey rounded-full h-1 mb-4 mt-4">
-                <div className="bg-neon-green h-1 rounded-full w-[50%]" ></div>
-            </div>
+            <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+            <input onChange={handleLength} id="default-range" type="range" min="0" max="20"  step="1" defaultValue={10} className="w-full h-2 bg-very-dark-grey rounded-lg border-none cursor-pointer accent-neon-green"/>
             <ul className="space-y-2 mt-4">
                 <li>
                     <Checkbox id="1" label="Include Uppercase Letters"/>
@@ -24,6 +34,7 @@ export default function Details() {
                     <Checkbox id="4" label="Include Symbols"/>
                 </li>
             </ul>
+            <Strength label="Medium"/>
             <button className="button mt-6 flex items-center justify-center">
                 {'Generate'.toUpperCase()}
                 <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" className="ml-2">
