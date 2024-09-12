ff8105280e34f2d8264fb46098b81c4a12dedb3f
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-09-12 12:04:19 -0500

    Fixing some styles

--- src/app/components/Checkbox.tsx
+++ src/app/components/Checkbox.tsx
@@ -12,7 +12,7 @@ export default function Checkbox(props: CheckboxProps) {
                     <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" className="absolute hidden peer-checked:block stroke-white outline-none pointer-events-none ml-0.5">
                         <path className="w-4 h-4" stroke="#18171F" strokeWidth="3" fill="none" d="M1 5.607 4.393 9l8-8"/>
                     </svg>
-                    <label className="ms-2 p">{props.label}</label>
+                    <label className="ms-2 p text-almost-white">{props.label}</label>
         </section>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Password from "./components/Password";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen px-[35%] flex-col items-center justify-center">
+    <main className="flex min-h-screen px-8 sm:px-[10%] md:px-[20%] lg:px-[25%] xl:px-[35%] flex-col items-center justify-center">
       <Password/>
       <Details/>
     </main>
